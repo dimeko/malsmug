2ada07009277c6ce84e46518af4e6ccd293db047
Dimitris Oikonomou <dimoiko100@gmail.com>
2025-04-02 22:31:30 +0300

    run sandbox with puppeteer user (non-root)

--- docker/js_sandbox_entrypoint.sh
+++ docker/js_sandbox_entrypoint.sh
@@ -1,7 +1,3 @@
 #!/bin/sh
 
-export DBUS_SESSION_BUS_ADDRESS=`dbus-daemon --fork --config-file=/usr/share/dbus-1/session.conf --print-address`
-
-/etc/init.d/dbus restart
-
 node /js_sandbox/lib/app.js $1 $2
